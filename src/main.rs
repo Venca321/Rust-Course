f20c99d2eb8510dcb4eb6809e61cda46465f5ab7
Václav Parma <login@vaclavparma.cz>
2024-04-25 19:03:52 +0200

    Add Lesson 2 homework

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,40 @@
-use std::io;
+//! Tohle je program vytvořený v Rustu na základě kurzu
+
+use slug::slugify;
+use std::{env, io};
 
 fn main() {
-    println!("Enter your name:");
+    println!("Zadejte vstup: ");
 
-    let mut name: String = String::new();
-
+    let mut input: String = String::new();
     io::stdin()
-        .read_line(&mut name)
+        .read_line(&mut input)
         .expect("Failed to read line");
 
-    let name = name.trim();
+    input = input.trim().to_string();
 
-    println!("Hello, {name}!");
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() < 2 {
+        println!("Input: {input}");
+        return;
+    }
+
+    println!("Args: {}", args[1]);
+
+    if args[1] == "lowercase" {
+        input = input.to_lowercase();
+    } else if args[1] == "uppercase" {
+        input = input.to_uppercase();
+    } else if args[1] == "no-spaces" {
+        input = input.replace(" ", "");
+    } else if args[1] == "slugify" {
+        input = slugify(&input);
+    } else if args[1] == "reverse" {
+        input = input.chars().rev().collect();
+    } else if args[1] == "snake_case" {
+        input = input.to_lowercase().replace(" ", "_");
+    }
+
+    println!("Input: {input}");
 }
